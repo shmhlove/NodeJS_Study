8452a47d189d06f3e0c20b675444f455d2657bf3
이상호 <shmhlove@naver.com>
2018-07-29 19:35:00 +0900

    [정리] 쳅터 별 폴더 구분

--- Do_it/Example/ch02/ch02_test4.js
+++ Do_it/Example/ch02/ch02_test4.js
@@ -15,5 +15,5 @@ console.log("모듈(type1) 분리 후 : %d", module.add(1, 2));
 module = require("./ch02_test4_module_type2");
 console.log("모듈(type2) 분리 후 : %d", module.add(1, 2));
 
-module = require("./ch02_module");
+module = require("./Module");
 console.log("index 모듈 분리 후 : %d", module.add(1, 2));
