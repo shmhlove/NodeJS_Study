7f402796ef2e6a32b2a87f704eecde088fb76c32
이상호 <shmhlove@naver.com>
2018-08-20 20:03:09 +0900

    [6장] MySQL (관리툴이 정상동작하지 않아 실습은 하지 않음)

---
+++ Do_it/Example/ch06/ch06_test6.js
@@ -0,0 +1,13 @@
+// < 데이터베이스 : MySQL 활용 >
+/*
+    * MySQL 설치
+        -> http://dev.mysql.com/downloads/
+        
+    * HeidiSQL 설치 : 관리도구 (mac용은 없다..)
+        -> http://www.heidisql.com/download.php
+        -> (mac용) https://www.toadworld.com/downloads
+        
+    * 테스트
+        -> sudo mongod --dbpath /data/db
+        -> http://127.0.0.1:3000/public/login.html
+*/
